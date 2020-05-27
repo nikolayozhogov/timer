ec0dccbf7c21ade059e9faee6281ccfe18ea62ea
Nikolay Ozhogov <nm.ozhogov@ya.ru>
2020-05-27 16:26:01 +0500

    Updates

---
+++ precache-manifest.70751ff0262afcad8e920c8a6de1c357.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babfa54dc41816880c81275122ad23e0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2eb1f77056669622b1fb",
+    "url": "./static/css/2.a28d9ceb.chunk.css"
+  },
+  {
+    "revision": "dbe546d7e410683e3b5d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2eb1f77056669622b1fb",
+    "url": "./static/js/2.8bf04f6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8bf04f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbe546d7e410683e3b5d",
+    "url": "./static/js/main.a2c6ad4f.chunk.js"
+  },
+  {
+    "revision": "71edaeb63c9b3c0daddd",
+    "url": "./static/js/runtime-main.43c88c3c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
